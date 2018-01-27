df87fa2a7038be759f329bea33864b5fb52b60ca
Christian Arce <carce@perceptiongrp.com>
2018-01-27 16:59:18 -0300

    Adding comments for explain exercise

--- web-crawler/download.js
+++ web-crawler/download.js
@@ -6,12 +6,24 @@ const uuidv1 = require('uuid/v1')
 const downloadPage = (url = 'http://nodeprogram.com') => {
     console.log('downloading ', url)
 
+    // function fetchPage, two arguments:
+    // urlF: url to fetch content
+    // callback: callback function to return something
     const fetchPage = (urlF, callback) => {
+
+        // Invoke HTTP Get from http module
+        // two arguments, url to get and callback function 
         http.get(urlF, (response) => {
+
+            // local variable like as buffer
             let buff = ''
+
+            // on event data
             response.on('data', (chunk) => {
                 buff += chunk
             })
+
+            // on event end
             response.on('end', () => {
                 callback(null, buff)
             })
@@ -21,8 +33,14 @@ const downloadPage = (url = 'http://nodeprogram.com') => {
         })
     }
 
+    //Generate from uuid unique folder name
     const folderName = uuidv1()
+
+    // Create folder
     fs.mkdirSync(folderName)
+
+
+    //Invoke fetchPage
     fetchPage(url, (error, data) => {
         if (error) return console.log(error)
         fs.writeFileSync(path.join(__dirname, folderName, 'url.txt'), url)
